1ecaeb02be7640f1380ebb442e1a66e03da7808d
Antoine <102359550+antoinea95@users.noreply.github.com>
2024-12-09 16:04:53 +0100

    update: add aria labels

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,19 +7,35 @@ export const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="text-2xl col-span-4 sm:col-span-6 lg:col-span-12 h-fit flex items-center justify-between">
-      {/* Logo */}
+    <header
+      className="text-2xl col-span-4 sm:col-span-6 lg:col-span-12 h-fit flex items-center justify-between"
+      role="banner"
+    >
+      {/* Logo & Navigation */}
       <div className="flex items-center justify-between lg:w-1/2">
-        <img src="./assets/navbar/stylenest.svg" alt="Stylenest logo" className="lg:w-32" />
+        <img
+          src="./assets/navbar/stylenest.svg"
+          alt="Stylenest logo"
+          className="lg:w-32"
+        />
         <Nav isOpen={isOpen} />
       </div>
 
-      {/* Action Buttons */}
-      <section className="flex items-center gap-4">
+      {/* Actions buttons */}
+      <section
+        className="flex items-center gap-4"
+        aria-label="Action buttons"
+      >
         {!isOpen && <ShoppingCartBtn />}
-          <button onClick={() => setIsOpen(!isOpen)} className="lg:hidden">
-            {isOpen ? <RiCloseLine /> : <RiMenuLine />}
-          </button>
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="lg:hidden focus:outline-none focus-visible:ring focus-visible:ring-black"
+          aria-controls="main-navigation"
+          aria-expanded={isOpen}
+          aria-label={isOpen ? "Close menu" : "Open menu"}
+        >
+          {isOpen ? <RiCloseLine /> : <RiMenuLine />}
+        </button>
       </section>
     </header>
   );
--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -8,22 +8,32 @@ export const Nav = ({ isOpen }: { isOpen: boolean }) => {
     "lg:static lg:translate-x-0 lg:w-fit lg:pointer-events-auto lg:h-fit";
 
   return (
-    <nav className={`text-base lg:text-xl ${mobileNav} ${desktopNav}`}>
-      <ul className="space-y-4 lg:flex lg:items-center lg:space-y-0 lg:gap-8">
-        <li className="relative group whitespace-nowrap">
-          <a href="#" className="">
+    <nav
+      className={`text-base lg:text-xl ${mobileNav} ${desktopNav}`}
+      aria-label="Main-navigation"
+    >
+      <ul
+        className="space-y-4 lg:flex lg:items-center lg:space-y-0 lg:gap-8"
+        role="menu"
+      >
+        <li className="relative group whitespace-nowrap" role="menuitem">
+          <a
+            href="#"
+            className="focus:outline-none focus-visible:ring focus-visible:ring-black"
+          >
             Shop all
           </a>
           <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-black transition-all duration-200 group-hover:w-full"></span>
         </li>
-        <li className="relative group whitespace-nowrap">
-        <a href="#Test" className="">
-          Latest arrivals
-        </a>
+        <li className="relative group whitespace-nowrap" role="menuitem">
+          <a
+            href="#Test"
+            className="focus:outline-none focus-visible:ring focus-visible:ring-black"
+          >
+            Latest arrivals
+          </a>
           <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-black transition-all duration-200 group-hover:w-full"></span>
         </li>
-
-        
       </ul>
     </nav>
   );
